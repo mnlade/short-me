ad2e6774a09beb292ac1882ce4d38503e5e2b9ab
Manuel Ade <mnlade@gmail.com>
2025-01-24 00:12:12 +0100

    remove unused router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { createLinkRouter} from "~/server/api/routers/shortener";
 /**
@@ -7,7 +6,6 @@ import { createLinkRouter} from "~/server/api/routers/shortener";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   createLinkRouter
 });
 
